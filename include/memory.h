5e97fb3f62cda3778ac7c5a88b2a0969d0b2b34e
brunozalc <brun0zalc@hotmail.com>
2024-06-24 15:42:02 -0300

    style: change to Google's C++ style

--- include/memory.h
+++ include/memory.h
@@ -10,6 +10,6 @@ class Memory {
   uint8_t read(uint16_t address) const;
   void write(uint16_t address, uint8_t value);
 
-  private:
-    std::array<uint8_t, 4096> memory; // CHIP-8 has 4KB of memory
+ private:
+  std::array<uint8_t, 4096> memory;  // CHIP-8 has 4KB of memory
 };
