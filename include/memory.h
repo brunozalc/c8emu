a11437ac007da8c779433eb8341359fd9468eb22
brunozalc <brun0zalc@hotmail.com>
2024-06-24 15:19:54 -0300

    feat: add Memory class for managing CHIP-8 memory

---
+++ include/memory.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <array>
+#include <cstdint>
+
+class Memory {
+ public:
+  Memory();
+  void load_rom(const char* filename);
+  uint8_t read(uint16_t address) const;
+  void write(uint16_t address, uint8_t value);
+
+  private:
+    std::array<uint8_t, 4096> memory; // CHIP-8 has 4KB of memory
+};
