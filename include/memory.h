7b8f70d76ddf0920e7b250eed0bb7b1630c1302b
brunozalc <brun0zalc@hotmail.com>
2024-06-27 14:28:31 -0300

    feat(memory): adds a method to load fonts

--- include/memory.h
+++ include/memory.h
@@ -3,12 +3,16 @@
 #include <array>
 #include <cstdint>
 
+#include "fonts.h"
+
 class Memory {
  public:
   Memory();
   void load_rom(const char* filename);
+  void load_font();
   uint8_t read(uint16_t address) const;
   void write(uint16_t address, uint8_t value);
+  const uint8_t* get_pointer(uint16_t address) const;
 
  private:
   std::array<uint8_t, 4096> memory;  // CHIP-8 has 4KB of memory
--- src/memory.cpp
+++ src/memory.cpp
@@ -1,5 +1,7 @@
 #include "memory.h"
 
+#include <stddef.h>
+
 #include <fstream>
 #include <iosfwd>
 #include <iostream>
@@ -24,8 +26,20 @@ void Memory::load_rom(const char* filename) {
   }
 }
 
+// method to load fontset into memory
+void Memory::load_font() {
+  for (size_t i = 0; i < fontset.size(); i++) {
+    memory[i] = fontset[i];
+  }
+}
+
 // method to read from memory
 uint8_t Memory::read(uint16_t address) const { return memory[address]; }
 
 // method to write to memory
 void Memory::write(uint16_t address, uint8_t value) { memory[address] = value; }
+
+// method to get a pointer to a memory address
+const uint8_t* Memory::get_pointer(uint16_t address) const {
+  return &memory[address];
+}
